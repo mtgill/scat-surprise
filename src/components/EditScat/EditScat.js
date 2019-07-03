a579bf81c491a6c59b03d93655e30acc974570dd
Matt Gill <mtgill90@outlook.com>
2019-07-02 20:34:20 -0500

    routing works

--- src/components/EditScat/EditScat.js
+++ src/components/EditScat/EditScat.js
@@ -4,9 +4,11 @@ import './EditScat.scss';
 
 class EditScat extends React.Component {
   render() {
+    const editId = this.props.match.params.id;
     return (
       <div className="EditScat">
         <h2>EditScat</h2>
+        <h3>The editId is {editId}</h3>
     </div>
     );
   }
