d64e05d93c299c523039082ec53aa4e54157b36f
Matt Gill <mtgill90@outlook.com>
2019-07-02 19:26:45 -0500

    routing works

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -11,6 +17,22 @@ import Auth from '../components/Auth/Auth';
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props}/>)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props}/>)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -33,16 +55,23 @@ class App extends React.Component {
 
   render() {
     const { authed } = this.state;
-    const loadComponent = () => {
-      if (authed) {
-        return <Home />;
-      }
-      return <Auth />;
-    };
+
     return (
       <div className="App">
-        <MyNavBar authed={authed}/>
-        {loadComponent()}
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavBar authed={authed} />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PublicRoute path="/auth" component={Auth} authed={authed} />
+                  <PrivateRoute path="/home" component={Home} authed={authed} />
+                  <Redirect from="*" to="/auth" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
