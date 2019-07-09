8006826443afb91c85697637cec6a742a790c173
Matt Gill <mtgill90@outlook.com>
2019-07-08 20:01:15 -0500

    read done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import scatData from '../../helpers/data/scatData';
+import ScatCard from '../ScatCard/ScatCard';
 
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    scats: [],
+  }
+
+  componentDidMount() {
+    const { uid } = firebase.auth().currentUser;
+    scatData.getScats(uid)
+      .then(scats => this.setState({ scats }))
+      .catch(err => console.error('could not get scats', err));
+  }
+
   editEvent = (e) => {
     e.preventDefault();
     const orderId = '12345';
@@ -11,12 +28,18 @@ class Home extends React.Component {
   };
 
   render() {
-    const singleLink = '/scat/12345';
+    const makeScatCards = this.state.scats.map(scat => (
+      <ScatCard
+      key={scat.id}
+      scat={scat}
+      />
+    ));
     return (
-      <div className="Home">
+      <div className="Home col">
         <h2>Home</h2>
-        <button className="btn btn-warning" onClick={this.editEvent}>Edit a thing</button>
-        <Link to={singleLink}>View Single</Link>
+        <div className="d-flex">
+        {makeScatCards}
+        </div>
     </div>
     );
   }
