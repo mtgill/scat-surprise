9f0805f937b62b0ea1b47f2e99e8b98dbaf881f7
Matt Gill <mtgill90@outlook.com>
2019-07-09 20:12:39 -0500

    wip

--- src/components/ScatCard/ScatCard.js
+++ src/components/ScatCard/ScatCard.js
@@ -22,6 +22,7 @@ class ScatCard extends React.Component {
   render() {
     const { scat } = this.props;
     const singleLink = `/scat/${scat.id}`;
+    const editLink = `/edit/${scat.id}`;
     return (
       <div className="ScatCard col-4">
         <div className="card">
@@ -29,6 +30,7 @@ class ScatCard extends React.Component {
             <h5 className="card-title">{scat.sampleName}</h5>
             <Link className="btn btn-success" to={singleLink}>View</Link>
             <p className="card-text">{scat.location}</p>
+            <Link className="btn btn-warning" to={editLink}>Update</Link>
             <button className="btn btn-danger" onClick={this.deleteMe}>Delete</button>
           </div>
         </div>
