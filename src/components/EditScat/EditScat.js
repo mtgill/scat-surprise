b72c8455c2b77b8a17863942ea38b6eb874a81e3
Matt Gill <mtgill90@outlook.com>
2019-07-09 20:20:29 -0500

    update works

--- src/components/EditScat/EditScat.js
+++ src/components/EditScat/EditScat.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
 
-import scatsData from '../../helpers/data/scatsData';
+import scatsData from '../../helpers/data/scatData';
 import './EditScat.scss';
 
 const defaultScat = {
